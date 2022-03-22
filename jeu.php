ec493655b5150741ff26ff50e34cfb3de009127e
NicolasBAELE <96698627+NicolasBAELE@users.noreply.github.com>
2022-03-22 14:12:09 +0100

    Update jeu.php

--- jeu.php
+++ jeu.php
@@ -1,23 +1,39 @@
 <?php
-
 $db = new PDO('pgsql:host=localhost;dbname=postgres', 'postgres', 'Niko250200');
 $db->query("SET NAMES 'utf8'");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$score=10;
-$pseudo=$_GET['pseudo'];
-$requete=$db->prepare("select count(*) as nb from pseudos where pseudo='$pseudo'");
-$requete->execute();
-$tab=$requete->fetch(PDO::FETCH_ASSOC);
+function entrerScore($db){
+    $score=100;
+    $pseudo=$_GET['pseudo'];
+    $requete=$db->prepare("select count(*) as nb from pseudos where pseudo='$pseudo'");
+    $requete->execute();
+    $tab=$requete->fetch(PDO::FETCH_ASSOC);
+    
+    if ($tab['nb']==0){
+        $requete=$db->prepare("insert into pseudos values ('$pseudo', '$score')");
+        echo "new<br>";
+    }
+    else {
+        $requete=$db->prepare("update pseudos set score='$score' where pseudo='$pseudo'");
+        echo "old<br>";
+    }
+    $requete->execute();
+}
 
-if ($tab['nb']==0){
-    $requete=$db->prepare("insert into pseudos values ('$pseudo', '$score')");
-    echo "new";
+
+function afficherTableauDesScores($db){
+    $requete=$db->prepare("select * from pseudos order by score desc limit 5");
+    $requete->execute();
+    $tab=$requete->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table>";
+    foreach($tab as $value){
+        echo "<tr><td>Pseudo : ".$value['pseudo']."</td><td>Score : ".$value['score']."</td></tr>";
+    }
+    echo "</table>";
 }
-else {
-    $requete=$db->prepare("update pseudos set score='$score' where pseudo='$pseudo'");
-    echo "old";
-}
-$requete->execute();
+
+entrerScore($db);
+afficherTableauDesScores($db);
 ?>
-
