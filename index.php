79564bf6010e051c1a4536165013f85ed632a6f0
LAOUAMENToufik <98191730+LAOUAMENToufik@users.noreply.github.com>
2022-03-22 09:45:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css.css">
+</head>
+<body>
+  <div class="container">
+    <table>
+      <thead>
+        <tr>
+           <th>Pseudo</th>
+           <th>score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Jeanne</td>
+          <td>Biche</td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    <form action="jeu.php" method="get" class="form-example">
+        <div class="login">
+          <label for="pseudo">Enter your pseudo: </label>   
+          <input type="text" name="pseudo" id="pseudo" required>
+        </div>
+  </div>
+</body>
+</html>
