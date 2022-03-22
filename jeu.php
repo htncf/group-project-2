f5124ac7e9b8dc2b5918ee44d2761762bc898093
NicolasBAELE <96698627+NicolasBAELE@users.noreply.github.com>
2022-03-22 12:04:44 +0100

    Update jeu.php

--- jeu.php
+++ jeu.php
@@ -1,1 +1,23 @@
+<?php
+
+$db = new PDO('pgsql:host=localhost;dbname=postgres', 'postgres', 'Niko250200');
+$db->query("SET NAMES 'utf8'");
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$score=10;
+$pseudo=$_GET['pseudo'];
+$requete=$db->prepare("select count(*) as nb from pseudos where pseudo='$pseudo'");
+$requete->execute();
+$tab=$requete->fetch(PDO::FETCH_ASSOC);
+
+if ($tab['nb']==0){
+    $requete=$db->prepare("insert into pseudos values ('$pseudo', '$score')");
+    echo "new";
+}
+else {
+    $requete=$db->prepare("update pseudos set score='$score' where pseudo='$pseudo'");
+    echo "old";
+}
+$requete->execute();
+?>
 
