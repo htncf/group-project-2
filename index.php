7904893be9a05cd1e20891e7c76224d926168f73
NicolasBAELE <96698627+NicolasBAELE@users.noreply.github.com>
2022-03-24 11:12:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -14,22 +14,32 @@
 </body>
 </html>
 <?php
-$db = new PDO('pgsql:host=localhost;dbname=postgres', 'postgres', 'password');
+try
+{
+	$db = new PDO('mysql:host=sql11.freesqldatabase.com:3306;dbname=sql11481224;charset=utf8', 'sql11481224', 'as7hUUwD7X');
+  $db->query("SET NAMES 'utf8'");
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "ok";
+}
+catch (Exception $e)
+{
+  die('Erreur : ' . $e->getMessage());
+}
 $db->query("SET NAMES 'utf8'");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 function afficherTableauDesScores($db){
-    $requete=$db->prepare("select * from pseudos order by score desc limit 5");
-    $requete->execute();
-    $tab=$requete->fetchAll(PDO::FETCH_ASSOC);
+  $requete=$db->prepare("select * from pseudos order by score desc limit 5");
+  $requete->execute();
+  $tab=$requete->fetchAll(PDO::FETCH_ASSOC);
 
-    echo '<div class="container">';
-    echo "<table><th>Pseudo</th><th>Score</th>";
-    foreach($tab as $value){
-        echo "<tr><td>".$value['pseudo']."</td><td>".$value['score']."</td></tr>";
-    }
-    echo "</table>";
-    echo "</div>";
+  echo '<div class="container">';
+  echo "<table><th>Pseudo</th><th>Score</th>";
+  foreach($tab as $value){
+    echo "<tr><td>".$value['pseudo']."</td><td>".$value['score']."</td></tr>";
+  }
+  echo "</table>";
+  echo "</div>";
 }
 
 afficherTableauDesScores($db);
